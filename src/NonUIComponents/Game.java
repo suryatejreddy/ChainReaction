5b0cc560574e9138a5eb3b950079e34f3d1706dd
Vishaal Udandarao <vishaal16119@iiitd.ac.in>
2017-10-07 02:40:31 +0530

    Added Game class without logic for game.

---
+++ src/NonUIComponents/Game.java
@@ -0,0 +1,116 @@
+package NonUIComponents;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game
+{
+    private ArrayList<Player> players;
+
+    public Game()
+    {
+        players=new ArrayList<Player>();
+        for(int i=0;i<players.size();i++)
+            players.set(i, new Player(i, new ArrayList<Cell>(), true));
+    }
+
+    public ArrayList<Player> getPlayers()
+    {
+        return players;
+    }
+
+    public boolean isGameOver()
+    {
+        int countOfPlayersAlive=0;
+        for(int i=0;i<players.size();i++)
+        {
+            if(players.get(i).isAlive())
+                countOfPlayersAlive++;
+        }
+        return countOfPlayersAlive==1;
+    }
+
+    public Player findWinner()
+    {
+        Player winner=players.get(0);
+        if(this.isGameOver())
+        {
+            for(int i=0;i<players.size();i++)
+            {
+                if(players.get(i).isAlive())
+                {
+                    winner=players.get(i);
+                    break;
+                }
+            }
+        }
+        return winner;
+    }
+
+    public void setPlayers(ArrayList<Player> players)
+    {
+        this.players = players;
+    }
+
+    private void playGame(Scanner scanner)
+    {
+        int i=0;
+        System.out.println("Enter X and Y.");
+        int x,y;
+        x=scanner.nextInt();
+        y=scanner.nextInt();
+        Matrix gameMatrix=new Matrix(x, y);
+        while(!isGameOver() && i<players.size())
+        {
+            System.out.println("Player "+(i+1)+"'s chance.");
+            System.out.println("Enter coordinates of matrix(0 based) to tap.");
+            Player currentPlayer= findCurrentPlayer(i);
+            int coordX, coordY;
+            coordX=scanner.nextInt();
+            coordY=scanner.nextInt();
+            Cell cellChosen=findChosenCell(coordX, coordY, gameMatrix);
+
+            if(!gameMatrix.checkIfCellIsFree(cellChosen))
+                continue;
+
+            gameMatrix.getCells().get(coordY).get(coordX).setCellIsOccupied(true);
+            gameMatrix.getCells().get(coordY).get(coordX).setPlayerOccupiedBy(currentPlayer);
+            gameMatrix.getCells().get(coordY).get(coordX).setNumberOfBallsPresent(gameMatrix.getCells().get(coordY).get(coordX).getNumberOfBallsPresent()+1);
+            ArrayList<Cell> occupiedCellsByCurrentPlayer=players.get(i).getCurrentOccupiedCells();
+            occupiedCellsByCurrentPlayer.add(cellChosen);
+            players.get(i).setCurrentOccupiedCells(occupiedCellsByCurrentPlayer);
+
+            if(i==players.size()-1)
+                i=0;
+            else
+                i++;
+        }
+    }
+
+    private Player findCurrentPlayer(int i)
+    {
+        return players.get(i);
+    }
+
+    private Cell findChosenCell(int coordX, int coordY, Matrix game)
+    {
+        return game.getCells().get(coordY).get(coordX);
+    }
+
+    public static void main(String[] args)
+    {
+        int numberOfPlayers;
+        System.out.println("Enter number of players.");
+        Scanner scanner=new Scanner(System.in);
+        numberOfPlayers=scanner.nextInt();
+        int i=0;
+        Game chainReactionGame=new Game();
+        while(i<numberOfPlayers)
+        {
+            Player player=new Player(i, new ArrayList<Cell>(), true);
+            chainReactionGame.getPlayers().add(player);
+            i++;
+        }
+        chainReactionGame.playGame(scanner);
+    }
+}
