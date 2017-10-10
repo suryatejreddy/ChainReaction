48f6addcc2e7127d1dca527e9c82d0b9fea217d5
Vishaal Udandarao <vishaal16119@iiitd.ac.in>
2017-10-10 20:54:56 +0530

    Added Game class(incomplete)

--- src/NonUIComponents/Player.java
+++ src/NonUIComponents/Player.java
@@ -5,29 +5,52 @@ import java.util.ArrayList;
 
 public class Player
 {
-    public static int VIOLET=0;
-    public static int BLUE=1;
-    public static int GREEN=2;
-    public static int YELLOW=3;
-    public static int ORANGE=4;
-    public static int RED=5;
-    public static int BROWN=6;
-    public static int BLACK=7;
+    public static final int VIOLET=0;
+    public static final int BLUE=1;
+    public static final int GREEN=2;
+    public static final int YELLOW=3;
+    public static final int ORANGE=4;
+    public static final int RED=5;
+    public static final int BROWN=6;
+    public static final int BLACK=7;
 
     private int playerColour;
     private ArrayList<Cell> currentOccupiedCells;
     private boolean isAlive;
+    private boolean takenFirstMove;
 
     public Player(int playerColour, ArrayList<Cell> currentOccupiedCells, boolean isAlive)
     {
         this.currentOccupiedCells=currentOccupiedCells;
         this.isAlive=isAlive;
         this.playerColour=playerColour;
+        this.takenFirstMove=false;
     }
 
     public int getPlayerColour()
     {
         return playerColour;
+    }
+
+    public boolean hasTakenFirstMove()
+    {
+        return takenFirstMove;
+    }
+
+    public String getPlayerColourByString()
+    {
+        switch(this.getPlayerColour())
+        {
+            case VIOLET:    return "Violet";
+            case BLUE:      return "Blue";
+            case GREEN:     return "Green";
+            case YELLOW:    return "Yellow";
+            case ORANGE:    return "Orange";
+            case RED:       return "Red";
+            case BROWN:     return "Brown";
+            case BLACK:     return "Black";
+        }
+        return null;
     }
 
     public ArrayList<Cell> getCurrentOccupiedCells()
@@ -53,5 +76,15 @@ public class Player
     public void setPlayerColour(int playerColour)
     {
         this.playerColour = playerColour;
+    }
+
+    public void setTakenFirstMove(boolean takenFirstMove)
+    {
+        this.takenFirstMove = takenFirstMove;
+    }
+
+    public void makeMove(int coordX, int coordY, Matrix gameMatrix, Game game, int i, Cell cellChosen, Player currentPlayer)
+    {
+        gameMatrix.getCells().get(coordY).get(coordX).burst(currentPlayer, gameMatrix, game, coordX, coordY, i, cellChosen);
     }
 }
