8b48b05083df31a7f743a7c4f55246e832e3be48
Suryatej Reddy <suryatej16102@iiitd.ac.in>
2017-10-28 11:08:34 +0400

    Code Cleaning, added functions for repetitive code

--- src/sample/ExtendedCell.java
+++ src/sample/ExtendedCell.java
@@ -193,72 +193,8 @@ public class ExtendedCell
         return temp;
     }
 
-    public void addSpheresToGroup(Group curGroup , Color color)
+    public void setTranslationToSphere(Sphere sphere)
     {
-        int size = curGroup.getChildren().size();
-        ArrayList<Sphere> newSpheres = getAllSpheres(color,size);
-        curGroup.getChildren().clear();
-        for (int i=0;i<size;i++){
-            Sphere curSphere = newSpheres.get(i);
-            curSphere.setTranslateX(i*10 + 5);
-            curSphere.setTranslateY(i*10 + 5);
-            curGroup.getChildren().add(curSphere);
-        }
-    }
-
-    public void addBall(ExtendedPlayer curPlayer)
-    {
-        curPlayer.setTakenFirstMove(true);
-
-        if (this.cellIsOccupied && this.playerOccupiedBy != curPlayer)  //cell is occupied by someone else
-        {
-            //TODO change color of the ball for UI
-            addSpheresToGroup(this.getGroup(),Main.getColor(curPlayer));
-//            System.out.println("Came to recursive call's beginning");
-            this.playerOccupiedBy.removeCell(this); //removing it from his list
-            getGroup().getChildren().add(getNewSphere(Main.getColor(curPlayer)));
-            return;
-//            for(int i=0;i<getGroup().getChildren().size();i++)
-//            {
-//                Sphere sphere=(Sphere) getGroup().getChildren().get(i);
-//                PhongMaterial phongMaterial = new PhongMaterial();
-//                phongMaterial.setDiffuseColor(Main.getColor(curPlayer));
-//                phongMaterial.setSpecularColor(Color.BLACK);
-//                sphere.setMaterial(phongMaterial);
-//                getGroup().getChildren().remove(i);
-//                switch (getGroup().getChildren().size())
-//                {
-//                    case 0:
-//                        sphere.setTranslateX(0);
-//                        break;
-//
-//                    case 1:
-//                        sphere.setTranslateX(10);
-//                        break;
-//
-//                    case 2:
-//                        sphere.setTranslateY(10);
-//                        sphere.setTranslateX(5);
-//                        break;
-//
-//                    default:
-//                        break;
-//                }
-//
-//                getGroup().getChildren().add(sphere);
-//                return;
-//
-//            }
-        }
-        this.setCellIsOccupied(true);
-        this.setPlayerOccupiedBy(curPlayer);
-        this.numberOfBallsPresent+=1;
-
-        Sphere sphere = new Sphere(10);
-        PhongMaterial phongMaterial = new PhongMaterial();
-        phongMaterial.setDiffuseColor(Main.getColor(curPlayer));
-        phongMaterial.setSpecularColor(Color.BLACK);
-        sphere.setMaterial(phongMaterial);
         switch (getGroup().getChildren().size())
         {
             case 0:
@@ -270,56 +206,79 @@ public class ExtendedCell
                 break;
 
             case 2:
-                sphere.setTranslateY(10);
                 sphere.setTranslateX(5);
+                sphere.setTranslateY(10);
                 break;
 
             default:
                 break;
         }
+    }
+
+    public void addBall(ExtendedPlayer curPlayer)
+    {
+        curPlayer.setTakenFirstMove(true);
+
+        if (this.cellIsOccupied && this.playerOccupiedBy != curPlayer)  //cell is occupied by someone else
+        {
+            //TODO change color of the ball for UI
+            this.playerOccupiedBy.removeCell(this); //removing it from his list
+            for(int i=0;i<getGroup().getChildren().size();i++)
+            {
+                Sphere sphere = getNewSphere(Main.getColor(curPlayer));
+                setTranslationToSphere(sphere);
+                getGroup().getChildren().remove(i);
+                getGroup().getChildren().add(sphere);
+            }
+        }
+        //NON_UI_PART
+        this.setCellIsOccupied(true);
+        this.setPlayerOccupiedBy(curPlayer);
+        this.numberOfBallsPresent+=1;
+
+        //UI PART, add a new Sphere
+        Sphere sphere = getNewSphere(Main.getColor(curPlayer));
+        setTranslationToSphere(sphere);
         getGroup().getChildren().add(sphere);
-        this.startRotation();
+
+        //Add the group in case it was not added
         if(getCell().getChildren().size()==0)
         {
-//            System.out.println("adding to empty box " + coordX + " , " + coordY);
             getCell().getChildren().add(group);
         }
+
+        this.startRotation();
+
+
         if (this.numberOfBallsPresent == this.getCriticalMass())
         {
 
-//            this.emptyCell();
-//            try
+            this.emptyCell();
+
+
+//            //part where i add animation
+//            this.getGroup().getChildren().clear();
+//            ArrayList<Sphere> allSpheres = getAllSpheres(Main.getColor(curPlayer),4);
+//            ParallelTransition mainTransition = new ParallelTransition();
+//            for (int i=0;i<this.neighbouringCells.size();i++)
 //            {
-//                //TODO add multi threading here
-//                this.neighbouringCells.forEach((ExtendedCell neighbour) ->
-//                {
-//                    neighbour.addBall(curPlayer);
-//                });
+//                Sphere curSphere = allSpheres.get(i);
+//                ExtendedCell neighbour = this.neighbouringCells.get(i);
+//                TranslateTransition move = new TranslateTransition();
+//                move.setDuration(Duration.seconds(2));
+//                move.setNode(curSphere);
+//                int moveX = neighbour.coordX - this.coordX;
+//                int moveY = neighbour.coordY - this.coordY;
+//                System.out.println(moveX + " " + moveY);
+//                move.setToX(moveX*60);
+//                move.setToY(moveY*60);
+//                this.cell.getChildren().add(curSphere);
+//                mainTransition.getChildren().add(move);
+//                neighbour.cell.toBack();
 //            }
-//            catch(StackOverflowError e)
-//            {
-//                System.out.println("Yayyyy.. You won.");
-//            }
-            this.getGroup().getChildren().clear();
-            ArrayList<Sphere> allSpheres = getAllSpheres(Main.getColor(curPlayer),4);
-            ParallelTransition mainTransition = new ParallelTransition();
-            for (int i=0;i<this.neighbouringCells.size();i++)
-            {
-                Sphere curSphere = allSpheres.get(i);
-                ExtendedCell neighbour = this.neighbouringCells.get(i);
-                TranslateTransition move = new TranslateTransition();
-                move.setDuration(Duration.seconds(2));
-                move.setNode(curSphere);
-                int moveX = neighbour.coordX - this.coordX;
-                int moveY = neighbour.coordY - this.coordY;
-                System.out.println(moveX + " " + moveY);
-                move.setToX(moveX*60);
-                move.setToY(moveY*60);
-                this.cell.getChildren().add(curSphere);
-                mainTransition.getChildren().add(move);
-                neighbour.cell.toBack();
-            }
 //            mainTransition.play();
+
+
             try
             {
                 this.neighbouringCells.forEach((ExtendedCell neighbour) ->
