1182cebf38581e96a7d05a65feef56f0ee3f9736
Vishaal Udandarao <vishaal16119@iiitd.ac.in>
2018-10-02 21:28:36 +0530

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -51,6 +51,9 @@ public class Main extends Application implements Serializable
     private static int currentX;
     private static int currentY;
 
+    public static idgaf;
+    public static func;
+
     public static final String SERIALIZE_RESUME_BOOL="serializeResumeBool.ser";
     public static final String SERIALIZE_QUEUE_FILE_UNDO="serializeQueueUndo.ser";
     public static final String SERIALIZE_GRID_FILE_UNDO="serializeGridUndo.ser";
